b1f7e8b589da981e5221c05a8873d62e6de2ecd3
marcusvrr <marcusvrr2@gmail.com>
2022-08-01 21:59:02 -0300

    exercicio1 concluido

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+const sacolao = []
+sacolao.push({
+    nome: 'Banana',
+    preco: 9,
+    disponibilidade: true
+},
+{
+    nome: 'Pera',
+    preco: 4.50,
+    disponibilidade: true
+},
+{
+    nome: 'Pitaya',
+    preco: 15.99,
+    disponibilidade: false
+});
+console.log(sacolao) // array com 3 objetos concluido
